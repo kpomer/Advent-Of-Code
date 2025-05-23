60b747d64ddd9fccba0f67ad7ea446de729de5fb
kpomer <pomerkevin@gmail.com>
2025-05-23 18:39:35 +0300

    2015 Day 5 part 1 complete

--- 2015/Days/day_05/day05.py
+++ 2015/Days/day_05/day05.py
@@ -16,10 +16,10 @@ Current_Dir = os.path.dirname(__file__) #directory of current folder
 
 
 def main():
-    fileSA = shr.fileAsStringArray(Current_Dir, "e")
+    fileSA = shr.fileAsStringArray(Current_Dir)
     # print(fileSA)
 
-    niceWords = {}
+    niceWords = set()
 
     #Nice Strings
     vowels = {"a","e","i","o","u"}
@@ -27,6 +27,7 @@ def main():
 
     #loop through strings in input
     for s in fileSA:
+        s = s.strip("\n")
         vowelCount = 0
         doubleLetters = False
 
@@ -36,13 +37,18 @@ def main():
             if(l != len(s) - 1):
                 if (str(s[l] + s[l+1]) in badStrings):
                     break
+                if (s[l] == s[l+1]):
+                    doubleLetters = True
 
-            if(l in vowels):
+            if(s[l] in vowels):
                 vowelCount += 1
 
             l += 1
             if(l == len(s) and vowelCount >= 3 and doubleLetters == True):
                 niceWords.add(s)
+
+    print(f"Part 1: {len(niceWords)}")
+
             
             
 
