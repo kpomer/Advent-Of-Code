fd8ca3a049b79f61bef10691c9ef2204c2318511
kpomer <pomerkevin@gmail.com>
2025-05-23 17:06:52 +0300

    2015 day 4 complete

--- 2015/Days/day_04/day04.py
+++ 2015/Days/day_04/day04.py
@@ -1,6 +1,7 @@
 # main.py
 import sys
 import os
+import hashlib
 
 # region Access Shared Functions
 # Get the absolute path to the directory containing the current script (main.py)
@@ -16,7 +17,27 @@ Current_Dir = os.path.dirname(__file__) #directory of current folder
 
 
 def main():
-    fileSA = shr.fileAsStringArray(Current_Dir, "e")
-    # print(fileSA)
+    file = shr.fileAsStringArray(Current_Dir)[0]
+    
+    ## PART 1
+    for d in range(10000000):
+        val = file + str(d)
+        hashVal = hashlib.md5(val.encode())
+        hexVal = hashVal.hexdigest()
+        
+        if(hexVal[:5] == "00000"):
+            print(f"Part 1: {d}")
+            break
+
+    ## PART 2
+    for d in range(100000000):
+        val = file + str(d)
+        hashVal = hashlib.md5(val.encode())
+        hexVal = hashVal.hexdigest()
+        
+        if(hexVal[:6] == "000000"):
+            print(f"Part 2: {d}")
+            break
+        
 
 main()
