a6790108877d4d1127e76640848602a5624bf622
kpomer <pomerkevin@gmail.com>
2025-05-24 10:32:05 +0300

    2015 Day 5 both parts completed

--- 2015/Days/day_05/day05.py
+++ 2015/Days/day_05/day05.py
@@ -21,6 +21,7 @@ def main():
 
     niceWords = set()
 
+    ## Part 1
     #Nice Strings
     vowels = {"a","e","i","o","u"}
     badStrings = {"ab","cd","pq","xy"}
@@ -48,6 +49,49 @@ def main():
                 niceWords.add(s)
 
     print(f"Part 1: {len(niceWords)}")
+
+
+    ## Part 2
+    niceWords.clear()
+    for s in fileSA:
+        s = s.strip("\n")
+
+        twoLetterPairs = False
+        repeatLetter = False
+        #loop through letters in string
+        l = 0
+        while(l < len(s) - 1):
+            l2 = l + 2
+            while(l2 < len(s)):
+                
+                if((repeatLetter == False or twoLetterPairs == False) and s[l] == s[l2]):
+                    ## still looking for 'nice' criteria
+
+                    if(repeatLetter == False and l2 == l + 2):
+                        repeatLetter = True
+
+                    if(twoLetterPairs == False and l2 > l + 1 and l2 != len(s) - 1):
+                        if(s[l+1] == s[l2+1]):
+                            twoLetterPairs = True
+
+                    if(repeatLetter == True and twoLetterPairs == True):
+                        niceWords.add(s)
+                        break
+                l2 += 1
+            if(repeatLetter == True and twoLetterPairs == True):
+                break
+            l += 1
+
+    print(f"Part 2: {len(niceWords)}")
+
+        
+
+                    
+
+
+
+
+
 
             
             
