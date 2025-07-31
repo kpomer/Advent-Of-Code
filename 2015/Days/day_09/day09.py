8c7c6a8926f9256be6e46caf4d21ff52299a70a6
kpomer <pomerkevin@gmail.com>
2025-07-31 21:52:36 +0300

    2015 day 9 part 1 complete

--- 2015/Days/day_09/day09.py
+++ 2015/Days/day_09/day09.py
@@ -1,6 +1,7 @@
 # main.py
 import sys
 import os
+import math
 
 # region Access Shared Functions
 # Get the absolute path to the directory containing the current script (main.py)
@@ -14,33 +15,101 @@ import util.sharedFunctions as shr #import shared functions from util folder
 # Global Variables
 Current_Dir = os.path.dirname(__file__) #directory of current folder
 
-class Route:
-  def __init__(self, pointA, pointB, distance):
-    self.p1 = pointA
-    self.p2 = pointB
-    self.dist = distance
+CostMatrix = dict()
+Nodes = set()
+NodeToNum = dict()
+DP = dict()
+
 
 def main():
-    fileSA = shr.fileAsStringArray(Current_Dir, "e")
-    
-    routes = []
-    nodes = set()
-    nodeDict = dict()
+    fileSA = shr.fileAsStringArray(Current_Dir)
+    initializeData(fileSA)
 
+    N = len(Nodes)
+    minCost_ForAll = float('inf')
+
+    for startNode in range(N):
+        DP = dict()
+
+        # Find distances from 0 to each other node
+        for j in range(0,N):
+            if (j == startNode):
+                continue
+            mask = 1 << startNode | 1 << j
+            # print(str(mask) + ": " + formatBin(mask, N))
+            if(mask not in DP):
+                DP[mask] = dict()
+
+            DP[mask][j] = getCost(startNode, j)
+
+        
+        for subset_size in range(3, N + 1):
+            # print("\n\n")
+            # print("Subset Size: " + str(subset_size))
+            for mask in range(1, 1 << N):
+                # print(str(mask) + ": " + formatBin(mask, N))
+                if bin(mask).count('1') == subset_size and ((mask >> startNode) & 1):
+                    # print("\n")
+                    for j in range(N):
+                        # print("j: " + str(j) + " - " + formatBin(j, N))
+                        if j == startNode or not ((mask >> j) & 1):
+                            continue
+                    
+                        prev_mask = mask ^ (1 << j)
+                        # print(str(prev_mask) + ": " + formatBin(prev_mask, N))
+
+                        minCostTo_j = float('inf') #start at infinite
+                        for i in range(N):
+                            if i != j and i != startNode and ((prev_mask >> i) & 1):
+                                if prev_mask in DP and i in DP[prev_mask]:
+                                    cost_i_to_j = DP[prev_mask][i] + getCost(i,j)
+                                    minCostTo_j = min(minCostTo_j, cost_i_to_j)
+                                else:
+                                    Exception("Error!")
+                        if mask not in DP:
+                            DP[mask] = {}
+                        DP[mask][j] = minCostTo_j
+
+        final_mask = (1 << N) - 1
+        for p in DP[final_mask]:
+            minCost_ForAll = min(minCost_ForAll, DP[final_mask][p])
+
+    print("Part 1: " + str(minCost_ForAll))
+    
+
+
+
+
+
+def initializeData(fileSA):
     for r in fileSA:
         r = str.replace(r, "\n", "")
         
         r_Split = r.split(" ")
-        routes.append(Route(r_Split[0], r_Split[2], r_Split[4]))
-        for i in {r_Split[0], r_Split[2]}:
-           if(i not in nodes):
-              nodeDict[len(nodes)] = i
-              nodes.add(i)
+        for n in [r_Split[0], r_Split[2]]:
+           if(n not in Nodes):
+              NodeToNum[n] = len(Nodes)
+              Nodes.add(n)
+        setCost(NodeToNum[r_Split[0]], NodeToNum[r_Split[2]], r_Split[4])
 
+        
 
+        
+   
 
-        # Pathfinding needed - maybe Dijkstra's or A*, but must check ALL nodes (not only shortest from A->Z)
-        # Traveling Salesman Problem - Held-Karp algorithm 
+def setCost(node1, node2, cost):
+   setMask = 1 << node1 | 1 << node2
+   CostMatrix[setMask] = int(cost)
 
+def getCost(node1, node2):
+   setMask = 1 << node1 | 1 << node2
+   return CostMatrix[setMask]
+
+def formatBin(val, length):
+    bVal = bin(val)
+    bVal = bVal.replace("0b", "")
+    while(len(bVal) < length):
+        bVal = "0" + bVal
+    return bVal
 
 main()
