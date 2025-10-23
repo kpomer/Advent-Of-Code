dfa2e43ed6f62068d0a86f1f9addb816e0cc6273
kpomer <pomerkevin@gmail.com>
2025-10-23 22:19:07 +0300

    day 10 part 2 complete - using LIST instead of concat string

--- 2015/Days/day_10/day10.py
+++ 2015/Days/day_10/day10.py
@@ -14,26 +14,33 @@ import util.sharedFunctions as shr #import shared functions from util folder
 # Global Variables
 Current_Dir = os.path.dirname(__file__) #directory of current folder
 
-
 def main():
     inputValue = shr.fileAsStringArray(Current_Dir)[0]
+
     processedValue = int(inputValue)
-    for i in range(40): # change to 40
+    for i in range(50):
         processedValue = lookAndSayConvert(str(processedValue))
 
-    print(f"Part 1: {len(str(processedValue))}")
+        if(i+1 == 40):
+            print(f"Part 1: {len(str(processedValue))}")
+        elif(i+1 == 50):
+            print(f"Part 2: {len(str(processedValue))}")
 
+    
 
 def lookAndSayConvert(valueToProcess):
-    newValue = ""
+    newValue = [] # storing as LIST instead of concatenating STRING on each step
     l = 0
     while l < len(valueToProcess):
         repeats = 1
         while (l+1 < len(valueToProcess) and valueToProcess[l] == valueToProcess[l+1]):
             repeats+=1
             l+=1
-        newValue += str(repeats) + valueToProcess[l]
+        newValue.append(str(repeats))
+        newValue.append(valueToProcess[l])
         l+=1
-    return int(newValue)
+
+    return "".join(newValue)
+
 
 main()
