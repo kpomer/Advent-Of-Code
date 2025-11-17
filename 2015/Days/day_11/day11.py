e2d35176ca82b480753b538c7ad0d0176f37fc97
kpomer <pomerkevin@gmail.com>
2025-11-17 18:48:02 +0200

    2015 Parts 1 and 2 completed.  Currently using brute force in under 5 seconds, but likely could be much faster with reworking

--- 2015/Days/day_11/day11.py
+++ 2015/Days/day_11/day11.py
@@ -16,7 +16,68 @@ Current_Dir = os.path.dirname(__file__) #directory of current folder
 
 
 def main():
-    fileSA = shr.fileAsStringArray(Current_Dir, "e")
-    # print(fileSA)
+    fileSA = shr.fileAsStringArray(Current_Dir)
+    password = fileSA[0].strip("\n")
+
+    # Part 1
+    while meetsRequirements(password) == False:
+        password = incrementPassword(password)
+    print("Part 1: " + password)
+
+    # Part 2
+    password = incrementPassword(password)
+    while meetsRequirements(password) == False:
+        password = incrementPassword(password)
+    print("Part 2: " + password)
+
+
+def meetsRequirements(passwordValue):
+    # Req 1 - 1+ increasing straight3
+    # Req 2 - exclude i, o, l
+    # Req 3 - 2+ non-overlapping pairs
+
+    #Req 2
+    for invalidChar in ["i","o","l"]:
+        if passwordValue.find(invalidChar) != -1:
+            return False
+        
+    #Req 1
+    straight3 = False
+    for l in range(len(passwordValue)-2):
+        if(ord(passwordValue[l]) == ord(passwordValue[l+1])-1):
+            if(ord(passwordValue[l+1]) == ord(passwordValue[l+2])-1):
+                straight3 = True
+                break
+    if straight3 == False:
+        return False
+    
+    
+    #Req 3
+    c = 0
+    pairCount = 0
+    while (pairCount < 2) and (c < len(passwordValue) - 1):
+        if passwordValue[c] == passwordValue[c+1]:
+            pairCount += 1
+            c+=1
+        c+=1
+    if pairCount < 2:
+        return False
+    
+    return True
+    
+    
+
+def incrementPassword(password):
+    passwordList = list(password)
+    i = len(passwordList) - 1
+    while passwordList[i] == "z":
+        passwordList[i] = "a"
+        i-=1
+    
+    passwordList[i] = chr(ord(passwordList[i])+1)
+    return "".join(passwordList)
+
+
+
 
 main()
