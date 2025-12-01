b15b25bec22277b9d3d5a828ce1024dab4b47a65
kpomer <pomerkevin@gmail.com>
2025-12-01 18:48:40 +0200

    2025 Day 1 Part 2 Completed

--- 2025/Days/day_01/day_01.py
+++ 2025/Days/day_01/day_01.py
@@ -20,6 +20,7 @@ def main():
 
     val = 50
     password1 = 0
+    password2 = 0
     for i in fileSA:
         instruction = i.replace("\n", "")
         dir = instruction[0]
@@ -29,14 +30,31 @@ def main():
         elif(dir == "L"):
             dist = int(instruction[1:])*-1
         
-        val+=dist
+        startVal = val
+        val+=dist # Turn the dial
 
-        # Normalize 0-99
-        val = val % 100
+        val = val % 100 # Normalize 0-99
 
+        # Part 1
         if val == 0:
             password1 += 1
 
+        # Part 2
+        if(dist == 0):
+            continue
+
+        laps = int(abs(dist) / 100) # how many full rotations
+        additionalClicks = abs(dist) % 100 
+        password2 += laps
+
+        if additionalClicks == 0 or startVal == 0:
+            continue
+        elif(dir == "R" and (startVal + additionalClicks) >= 100):
+            password2 += 1
+        elif(dir == "L" and (startVal - additionalClicks) <= 0):
+            password2 += 1
+
     print(f"Part 1: {password1}")
+    print(f"Part 2: {password2}")
 
 main()
