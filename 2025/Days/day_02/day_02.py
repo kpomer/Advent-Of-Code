d1839251703dc612a26094cd5d287d29b8f6e915
kpomer <pomerkevin@gmail.com>
2025-12-02 11:36:16 +0200

    2025 Day 2 Part 2 complete

--- 2025/Days/day_02/day_02.py
+++ 2025/Days/day_02/day_02.py
@@ -13,36 +13,75 @@ import util.sharedFunctions as shr #import shared functions from util folder
 
 # Global Variables
 Current_Dir = os.path.dirname(__file__) #directory of current folder
+divCache = {}
 
 
 def main():
     fileSA = shr.fileAsStringArray(Current_Dir)[0].split(",")
 
-    part1_sum = 0
+    part1_invalid = set()
+    part2_invalid = set()
     for r in fileSA:
         range = r.split("-")
         start = int(range[0])
-        end = (range[1])
+        end = int(range[1])
 
-        c = int(start)
-        while c <= int(end):
-            idLength = len(str(c))
-            if idLength % 2 == 0:
-                splitLength = int(idLength/2)
-                c_Str = str(c)
-                if c_Str[:splitLength] == c_Str[splitLength:]:
-                    part1_sum += c
-            
-            c+= 1
+        idLength = len(str(start))
+        while idLength <= len(str(end)):
+            for d in divisors(idLength):
+                repeatVal = "1" + "0"*(d-1)
+                while len(repeatVal) == d:
+                    checkNum = combinedNum(repeatVal, int(idLength/d))
+                    if checkNum > end:
+                        # Exceeded END value
+                        break
+                    if(checkNum >= start):
+                        # ID is within range
+                        if int(idLength/d) == 2:
+                            # Part1 - invalid only if repeated twice
+                            part1_invalid.add(checkNum)    
+                        # Part2 - invalid for any number of repeats
+                        part2_invalid.add(checkNum)
+                    repeatVal = str(int(repeatVal) + 1)
 
+            idLength += 1
+
+
+    # Part 1
+    part1_sum = 0
+    for s in part1_invalid:
+        part1_sum += s
     print(f"Part 1: {part1_sum}")
 
-        # idLength = len(start)
-        # while idLength <= len(end):
-        #     if idLength % 2 == 0:
-        #         # Even number of digits
-        #         id_1 = start
-            
-        #     idLength += 1
+    # Part 2
+    part2_sum = 0
+    for s in part2_invalid:
+        part2_sum += s
+    print(f"Part 2: {part2_sum}")
+
+
+
+def divisors(num):
+    if num in divCache:
+        return divCache[num]
+    
+    values = []
+    v = 1
+    while v <= int(num/2):
+        if num % v == 0:
+            values.append(v)
+
+        v += 1
+
+    divCache[num] = values
+    return values
+
+
+def combinedNum(repeatVal, count):
+    strVal = ""
+    for c in range(count):
+        strVal += repeatVal
+    
+    return int(strVal)
 
 main()
