7cc3e42364b584213f71259a006437eee572f686
kpomer <pomerkevin@gmail.com>
2025-12-11 22:43:34 +0200

    2025 Day 6 Part 1 Complete

--- 2025/Days/day_06/day_06.py
+++ 2025/Days/day_06/day_06.py
@@ -1,6 +1,7 @@
 # main.py
 import sys
 import os
+import math
 
 # region Access Shared Functions
 # Get the absolute path to the directory containing the current script (main.py)
@@ -16,30 +17,34 @@ Current_Dir = os.path.dirname(__file__) #directory of current folder
 
 
 def main():
-    fileSA = shr.fileAsStringArray(Current_Dir, "e")
+    fileSA = shr.fileAsStringArray(Current_Dir)
     
     mathProblems = [] # [num1, num2, ..., numN, symbol]
+    problemElements = len(fileSA) - 1
+    
     row = 0
-
-    while row < 4:
+    while row <= problemElements:
         rowVal = fileSA[row].replace("\n","").split()
         col = 0
         while col < len(rowVal):
             if row == 0:
-                mathProblems.append([int(rowVal[col]),0,0,""])
-            elif row == 1 or row == 2:
-                mathProblems[col][row] = int(rowVal[col])
-            elif row == 3:
-                mathProblems[col][row] = rowVal[col]
+                mathProblems.append([0] * (problemElements+1))
+            val = rowVal[col]
+            if val.isnumeric():
+                mathProblems[col][row] = int(val)
+            else:
+                mathProblems[col][row] = val
             col += 1
         row += 1
 
     part1_sum = 0
     for problem in mathProblems:
-        if problem[3] == "+":
-            part1_sum += (problem[0] + problem[1] + problem[2])
-        elif problem[3] == "*":
-            part1_sum += (problem[0] * problem[1] * problem[2])
+        if problem[problemElements] == "+": 
+            problem.pop(problemElements)   
+            part1_sum += sum(problem)
+        elif problem[problemElements] == "*":
+            problem.pop(problemElements)   
+            part1_sum += math.prod(problem)
 
     print(f"Part 1: {part1_sum}")
 
